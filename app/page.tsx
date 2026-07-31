4e5410f10ce4809f8cfb41d922040fdf316d4ef0
Shish <shishfruitwala29@gmail.com>
2026-07-31 16:31:29 +0530

    intial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to SolveIt</h1>
+      <a href="/signup">Sign Up</a>
+      {" | "}
+      <a href="/login">Log In</a>
+    </div>
+  );
+}
