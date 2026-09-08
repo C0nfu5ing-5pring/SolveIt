740d12f50fee0b5afc232cd2f6bd765d35f743ad
Shish <shishfruitwala29@gmail.com>
2026-09-08 08:12:19 +0530

    fix: getMyPapers bug fix

--- server/controllers/papers.controller.js
+++ server/controllers/papers.controller.js
@@ -174,10 +174,10 @@ export const getPaperById = async (req, res) => {
 export const getMyPapers = async (req, res) => {
   try {
     const [papers] = await db.query(
-      "SELECT * FROM papers WHERE id = ? ORDER BY uploaded_at DESC",
-      [req, userID],
+      "SELECT * FROM papers WHERE uploaded_by = ? ORDER BY uploaded_at DESC",
+      [req.userId],
     );
-    return res.status(500).json({ success: true, papers });
+    return res.json({ success: true, papers });
   } catch (err) {
     console.error(err);
     return res.status(500).json({
@@ -186,6 +186,8 @@ export const getMyPapers = async (req, res) => {
     });
   }
 };
+
+
 
 export const deletePaper = async (req, res) => {
   try {
