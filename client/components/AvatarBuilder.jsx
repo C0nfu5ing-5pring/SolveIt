ba6a88dbc3798c6e1b3c32a6824076af63a8e155
Shish <shishfruitwala29@gmail.com>
2026-09-11 16:05:09 +0530

    fix: add eyes

--- client/components/AvatarBuilder.jsx
+++ client/components/AvatarBuilder.jsx
@@ -99,9 +99,26 @@ export default function AvatarBuilder({ currentConfig, onClose, onSave }) {
         </div>
 
         <div>
+          <p className="text-lg mb-2">Eyes</p>
+          <div className="flex gap-2 flex-wrap">
+            {eyes.map((eye) => (
+              <button
+                key={eye}
+                onClick={() => setConfig((prev) => ({ ...prev, eyes: eye }))}
+                className={`px-3 py-1 rounded-xl text-lg sketchy-border cursor-pointer active:scale-95 transition-all capitalize ${
+                  config.eyes === eye ? "bg-[#171717] text-[#fffef9]" : ""
+                }`}
+              >
+                {eye}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <div>
           <p className="text-lg mb-2">Mouth</p>
           <div className="flex gap-2 flex-wrap">
-            {mouth_options.map((mouth) => (
+            {mouth.map((mouth) => (
               <button className="flex gap-2 flex-wrap">{mouth}</button>
             ))}
           </div>
